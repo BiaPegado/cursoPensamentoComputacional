77f9926cf0ae72d107548b02012433716b089451
BiaPegado <apegado014@gmail.com>
2024-09-24 14:34:15 -0300

    Adiciona arquivo listas_01.py

---
+++ Estrutura_de_dados/listas_01.py
@@ -0,0 +1,9 @@
+
+
+lista_vazia = []
+lista_str = list("macarrão")
+lista_compras = ['arroz, feijão, carne, frango']
+
+print(lista_vazia)      #[]
+print(lista_str)        #['m', 'a', 'c', 'a', 'r', 'r', 'ã', 'o']
+print(lista_compras)    #['arroz, feijão, carne, frango']
