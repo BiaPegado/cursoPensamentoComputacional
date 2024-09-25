906e9b93e3900fd214d7d4a48e09f2a49c97dc3e
BiaPegado <apegado014@gmail.com>
2024-09-25 13:16:55 -0300

    Adiciona arquivo pesquisa_binaria.py

---
+++ Estrutura_de_dados/pesquisa_binaria.py
@@ -0,0 +1,19 @@
+def pesquisa_binaria(lista_ordenada, item):
+    baixo = 0
+    alto = len(lista_ordenada)-1
+    while baixo <= alto:
+        meio = (baixo+alto)//2
+        chute = lista_ordenada[meio]
+        if(chute == item):
+            return meio
+        if(chute > item):
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+
+lista_ordenada = [0, 2, 4, 6, 8, 10]
+print(pesquisa_binaria(lista_ordenada, 9))
+print(pesquisa_binaria(lista_ordenada, 0))
+print(pesquisa_binaria(lista_ordenada, 8))
+print(pesquisa_binaria(lista_ordenada, 1))
