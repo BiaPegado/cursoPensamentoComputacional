e83eba0d65930d755bebcfec9d2cba89e729c8ca
BiaPegado <apegado014@gmail.com>
2024-10-10 13:13:41 -0300

    Adiciona exemplo de SHA

---
+++ Estrutura_de_dados/10_SHA.py
@@ -0,0 +1,15 @@
+import hashlib
+
+def criptografar(texto):
+   
+    hash_sha256 = hashlib.sha256()
+    hash_sha256.update(texto.encode())
+    
+    return hash_sha256.hexdigest()
+
+texto = "Senha123"
+texto2 = "senha123"
+valorHash = criptografar(texto)
+valorHash2 = criptografar(texto2)
+print(f"\nO hash SHA-256 da mensagem '{texto}' é: \n{valorHash}")
+print(f"O hash SHA-256 da mensagem '{texto2}' é: \n{valorHash2}\n")
