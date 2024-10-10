9e924c075d82eb0d469c4a60c93d76c351f55231
BiaPegado <apegado014@gmail.com>
2024-10-10 12:23:10 -0300

    Adiciona exemplos de MapReduce

---
+++ Estrutura_de_dados/09_MapReduce_01.py
@@ -0,0 +1,17 @@
+# Para rodar: python 09_MapReduce_01.py input.txt
+from mrjob.job import MRJob
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+class MRWordFreqCount(MRJob):
+
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), 1)
+
+    def reducer(self, word, counts):
+        yield (word, sum(counts))
+
+if __name__ == '__main__':
+     MRWordFreqCount.run()
---
+++ Estrutura_de_dados/09_MapReduce_02.py
@@ -0,0 +1,25 @@
+#Para rodar: python 09_MapReduce_02.py empresas.csv
+
+from mrjob.job import MRJob
+
+class MRTotalFaturamento(MRJob):
+
+    def mapper(self, _, line):
+        # Ignorando a linha de cabeçalho
+        if line.startswith("Ano"):
+            return
+
+        # Dividindo a linha em colunas
+        columns = line.split(',')
+        nome_empresa = columns[1]
+        faturamento = float(columns[3])
+
+        # Emitindo o nome da empresa e o faturamento
+        yield (nome_empresa, faturamento)
+
+    def reducer(self, nome_empresa, faturamentos):
+        # Somando os faturamentos por empresa
+        yield (nome_empresa, sum(faturamentos))
+
+if __name__ == '__main__':
+    MRTotalFaturamento.run()
